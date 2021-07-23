ba9c7ad50197447244fde41f50823aaf8563f34e
Shailyusuf <habeebkwt57@gmail.com>
2021-07-24 01:56:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-rphncz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-rphncz)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';'classbtn'
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = {h1};however
